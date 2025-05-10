d6c5989fb86ca0382ab84ecc16ce14b55ed18dc5
zhouwei <zhouwei@chances.com.cn>
2025-05-10 22:38:21 +0800

    feat(config): LangSmith 配置、简单 LLM Application 实战

--- config/src/config/settings.py
+++ config/src/config/settings.py
@@ -3,10 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
     """
     全局配置管理，自动加载 .env 文件。
     """
+    # LangSmith 配置
+    LANGSMITH_TRACING: str = os.getenv("LANGSMITH_TRACING", "")
+    LANGSMITH_API_KEY: str = os.getenv("LANGSMITH_API_KEY", "")
+    LANGSMITH_PROJECT: str = os.getenv("LANGSMITH_PROJECT", "")
+
+    # DeepSeep API KEY
     DEEPSEEK_API_KEY: str = os.getenv("DEEPSEEK_API_KEY", "")
 
-settings = Settings() 
+
+settings = Settings()
