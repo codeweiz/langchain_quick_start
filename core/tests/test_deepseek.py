56416ea5f74f099bf119c059a08b1b57c79d01f7
zhouwei <zhouwei@chances.com.cn>
2025-05-10 16:26:18 +0800

    feat(tests): core 下 model 单元测试

--- core/tests/test_deepseek.py
+++ core/tests/test_deepseek.py
@@ -1,6 +1,21 @@
+from langchain_core.messages import BaseMessage
+
 from core.llm.deepseek import get_deepseek_chat_model
 
+
+# 校验 deepseek 模型
 def test_get_deepseek_chat_model():
     model = get_deepseek_chat_model()
+
+    # 校验模型加载成功
     assert model is not None
-    # 可根据实际模型类型进一步断言 
+
+
+# 校验模型 invoke
+def test_chat_model_invoke():
+    model = get_deepseek_chat_model()
+
+    # 校验模型可以 invoke 普通 str
+    message: BaseMessage = model.invoke("中国的首都是哪里？不需要介绍")
+    print(message.content)
+    assert message.content == '北京'
