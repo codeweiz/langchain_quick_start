aba44c21f04a146d67240b24e736d6c87af261cf
zhouwei <zhouwei@chances.com.cn>
2025-05-12 21:35:44 +0800

    feat(chat logprobs): 对数概率

---
+++ core/tests/test_logprobs.py
@@ -0,0 +1,13 @@
+from core.llm.deepseek import get_deepseek_chat_model
+
+
+# 测试获取对数概率
+def test_logprobs():
+    model = get_deepseek_chat_model()
+    model.bind(logprobs=True)
+
+    # DeepSeek Chat Model 不支持 logprobs
+    # OPENAI Chat Model 支持
+    response = model.invoke(("human", "how are you today"))
+    logprobs = response.response_metadata["logprobs"]
+    print(f"logprobs: {logprobs}")
